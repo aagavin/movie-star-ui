733b1740694a889fb0736d73421f29272411511f
Aaron <aagavin@gmail.com>
2022-03-13 20:47:20 -0400

    error checking on create account

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -17,35 +17,57 @@ import {
   IonGrid,
   IonRow,
   IonBackButton,
+  useIonAlert,
 } from "@ionic/react";
 import { useEffect, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, Auth } from "firebase/auth";
+import { RouteComponentProps } from "react-router-dom";
 
-const CreateAccount: React.FC = () => {
+interface LoginProps extends RouteComponentProps<{}> {}
 
+const CreateAccount: React.FC<LoginProps> = ({ history }) => {
   const [userName, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [passwordRep, setPasswordRep] = useState<string>("");
+  const [auth, setAuth] = useState<Auth>();
 
-  useEffect(()=>{
+  const [present] = useIonAlert();
+
+  useEffect(() => {
     const auth = getAuth();
-    console.log('------>');
-    console.log(auth.currentUser);
-  }, []);
+    setAuth(auth);
+    if (auth.currentUser) {
+      history.replace("/");
+    }
+  }, [history]);
+
+
+  const validateEmail = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const newAccount = () => {
-    const auth = getAuth();
-    if (auth.currentUser !== null) {
-      auth.signOut().then(console.log).catch(console.error);
-    }
-    createUserWithEmailAndPassword(auth, userName, password)
-      .then((userCred) => {
-        const user = userCred.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        console.log(error);
+    if (userName.length > 1 && !validateEmail(userName)){
+      present({
+        message: "Email is not valid!",
+        buttons: ["Ok"],
       });
+      return
+    }
+    if (password !== passwordRep) {
+      present({
+        message: "Password & Confirm Password don't match",
+        buttons: ["Ok"],
+      });
+    } else {
+      createUserWithEmailAndPassword(auth!, userName, password)
+        .then()
+        .catch(console.error);
+    }
   };
 
   return (
@@ -65,44 +87,47 @@ const CreateAccount: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            <IonList>
-              <IonItem>
-                <IonInput
-                  onIonChange={(e) => setUsername(e.detail.value!)}
-                  type="email"
-                  placeholder="Username"
-                />
-              </IonItem>
+              <IonList>
+                <IonItem>
+                  <IonInput
+                    onIonChange={(e) => setUsername(e.detail.value!)}
+                    pattern="email"
+                    inputMode="email"
+                    required
+                    type="email"
+                    placeholder="Username"
+                  />
+                </IonItem>
 
-              <IonItem>
-                <IonInput
-                  onIonChange={(e) => setPassword(e.detail.value!)}
-                  type="password"
-                  placeholder="Password"
-                />
-              </IonItem>
+                <IonItem>
+                  <IonInput
+                    onIonChange={(e) => setPassword(e.detail.value!)}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </IonItem>
 
-              <IonItem>
-                <IonInput
-                  onIonChange={(e) => setPasswordRep(e.detail.value!)}
-                  type="password"
-                  placeholder="Password repeat"
-                />
-              </IonItem>
+                <IonItem>
+                  <IonInput
+                    onIonChange={(e) => setPasswordRep(e.detail.value!)}
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                </IonItem>
 
-              <IonGrid>
-                <IonRow>
-                  <IonCol>
-                    <IonButton onClick={newAccount} expand="block">
-                      Create
-                    </IonButton>
-                  </IonCol>
-                  <IonCol>
-                    <IonButton expand="block">Clear</IonButton>
-                  </IonCol>
-                </IonRow>
-              </IonGrid>
-            </IonList>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonButton onClick={newAccount} expand="block">
+                        Create
+                      </IonButton>
+                    </IonCol>
+                    <IonCol>
+                      <IonButton expand="block">Clear</IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonList>
           </IonCardContent>
         </IonCard>
       </IonContent>
